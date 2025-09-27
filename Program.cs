d3f754d6428e96a1d9cecf64e724dbaf96e9e91b
João Coriliano <joaopedro.coriliano@gmail.com>
2025-09-26 22:26:28 -0300

    Realizando atividade

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("---Milhas para KM---");
+Console.Write("Informe o valor em milhas: ");
+string entrada = Console.ReadLine();
+double UmaMilhaEmKM = 1.609;
+double Milhas = Convert.ToDouble(entrada);
+double resultado = Milhas * 1.609;
+Console.WriteLine($"{Milhas} milhas em km --> {resultado}");
